f4c8476cd5b3a2aaf4ba3207f68048b5fdb66f80
Krish <reachklsd@gmail.com>
2022-04-27 18:55:50 -0400

    Version 1 of the SFO Foodtruck Finder

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from "react";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+import truckData from "./data/csvjson.json";
+import mapStyles from "./mapStyles";
+import * as geolib from 'geolib';
+
+function SFOMap() {
+  let trucks = new Map();
+  const maxNoOfSuggestions=7;
+  //load the foodTruck data and map required props
+  truckData.forEach(truck => {
+    trucks.set(truck.locationid,
+      {
+        locationId: truck.locationid,
+        foodItem: truck.foodItems,
+        address:truck.address,
+        name: truck.applicant,
+        latitude: truck.latitude,
+        longitude: truck.longitude
+      });
+  });
+  const [selectedTruck, setSelectedTruck] = useState(null);
+  const [trucksCloseBy, setTrucksCloseBy] = useState(null);
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectedTruck(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
+  return (
+    <GoogleMap
+      defaultZoom={14}
+      defaultCenter={{ lat: 37.79218368678335, lng: - 122.4043141170555 }}
+      defaultOptions={{ styles: mapStyles }}
+      onClick={(mapsMouseEvent) => {
+        const myLocation = mapsMouseEvent.latLng.toJSON();
+        //clear the state when user clicked at new latlng.
+        setTrucksCloseBy(null);
+        setSelectedTruck(null);
+        //set the set 
+        setTrucksCloseBy(geolib.orderByDistance(myLocation, Array.from(trucks.values())).slice(0, maxNoOfSuggestions));
+      }}
+    >
+      {trucksCloseBy && (trucksCloseBy.map(truck => (
+        <Marker
+          key = {truck.locationId}
+          position={{
+            lat: truck.latitude,
+            lng: truck.longitude
+          }}
+          onClick={() => {
+            setSelectedTruck(truck);
+          }}
+          icon={{
+            url: `/ftc.png`,
+            scaledSize: new window.google.maps.Size(20, 20)
+          }}
+        />
+      )))}
+      {selectedTruck && (
+        <InfoWindow
+          onCloseClick={() => {
+            setSelectedTruck(null);
+          }}
+          position={{
+            lat: selectedTruck.latitude,
+            lng: selectedTruck.longitude
+          }}
+        >
+          <div>
+            <h2>{selectedTruck.name}</h2>
+            <p>{selectedTruck.address}</p>
+            <p>{selectedTruck.foodItem}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
+}
+
+const MapWrapped = withScriptjs(withGoogleMap(SFOMap));
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <MapWrapped
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY
+          }`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
---
+++ src/mapStyles.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    featureType: "administrative",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#1f8a70"
+      },
+      {
+        weight: 0.7
+      }
+    ]
+  }
+  
+];
